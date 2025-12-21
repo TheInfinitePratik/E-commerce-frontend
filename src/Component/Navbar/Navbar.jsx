8cf38c865b075c5312c09ca22481d4ae640a0bd1
Pratik Mishra <pratikmishra422@gmail.com>
2025-12-21 17:48:39 +0530

    Updated brand name to BuyKart and footer changes

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -14,7 +14,7 @@ const {getTotalCartItems} = useContext(ShopContext);
     <div className='navbar'>
         <div className="nav-logo">
             <img src={logo} alt="" />
-            <p>SHOPPER</p>
+            <p>BuyKart</p>
         </div>
         <ul className="nav-menu">
             <li onClick={()=>{setMenu("shop")}} ><Link style={{textDecoration: 'none'}} to='/'>Shop</Link>{menu==="shop"?<hr/>:<></>}</li>
